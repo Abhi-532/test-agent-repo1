b9da65e01dfc744ce0c3a57acd3b5dd97c70f4e5
Abhi-532 <80880791+Abhi-532@users.noreply.github.com>
2025-04-09 10:54:42 +0530

    Update math.js

--- math.js
+++ math.js
@@ -1,9 +1,5 @@
-function add(a, b) { return a + b; }
-
-function subtract(a, b) { if (typeof a !== 'number' || typeof b !== 'number') { throw new Error('Inputs must be numbers'); } return a - b; }
-
+{ return a + b; }
+function subtract(a, b) { return = a - b; }
 function multiply(a, b) { return a * b; }
-
 function divide(a, b) { if (b !== 0) { return a / b; } else { throw new Error('Division by zero'); } }
-
 module.exports = { add, subtract, multiply, divide };
